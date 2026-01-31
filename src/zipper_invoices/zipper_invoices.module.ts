b4745777d370d8193b977a2a92494e62770f37b3
Остапенко Игорь <igorostap21.31@gmail.com>
2026-01-31 19:19:29 +0500

    fix

--- src/zipper_invoices/zipper_invoices.module.ts
+++ src/zipper_invoices/zipper_invoices.module.ts
@@ -3,9 +3,10 @@ import { ZipperInvoicesService } from './zipper_invoices.service';
 import { ZipperInvoicesController } from './zipper_invoices.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ZipperInvoices } from './entities/zipper_invoice.entity';
+import { ZippersService } from 'src/zippers/zippers.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ZipperInvoices])],
+  imports: [TypeOrmModule.forFeature([ZipperInvoices]),ZippersService],
   controllers: [ZipperInvoicesController],
   providers: [ZipperInvoicesService],
 })
